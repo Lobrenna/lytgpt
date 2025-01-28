db74ac8141145bb8f9fc7f3f7ba6de82af4f8530
Lobrenna <lars.ove.brenna@gmail.com>
2025-01-28 07:28:55 +0100

    Oppdater lytgptfront.js med nye funksjoner

--- lytgptfront.js
+++ lytgptfront.js
@@ -295,9 +295,11 @@ async function sendMessage(chatId, message) {
             console.log("sendMessage: Response from backend:", data);
             if (Object.keys(data).includes(selectedLongContext)) {
               // Send til /rag-endepunktet
+              console.log("sendMessage: Sending to /rag-endpoint");
               url = `${API_BASE_URL}/chats/${encodedChatId}/rag`;
               formData.append('long_context_selection', selectedLongContext);
             } else {
+              console.log("sendMessage: Sending to /messages-endpoint");
               // Send til vanlig håndtering
               formData.append('long_context_selection', selectedLongContext);
             }
